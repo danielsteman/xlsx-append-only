57451e44f685ed497b57cad6eb741420c3bea94e
Daniel Steman <daniel-steman@live.nl>
2024-04-15 19:49:25 +0200

    add logo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Upload
 
 Leverage [s3-upload](https://next-s3-upload.codingvalue.com/).
+
+## Design
+
+Something like [this](https://thirdweb.com/).
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+"use client";
+
+import { Flex, Grid, GridItem, useTheme } from "@chakra-ui/react";
+import Logo from "./components/logo";
 
 export default function Home() {
+  const theme = useTheme();
+  const maxWidth = theme.breakpoints.xl;
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap={4} p={4} mx="auto" maxW="48em">
+    <Grid
+      bgColor="black"
+      height="100%"
+      templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+      gap={4}
+      p={4}
+      mx="auto"
+      maxWidth={maxWidth}
+    >
+      <GridItem colSpan={{ base: 1, md: 2 }} bgColor="teal">
+        <Flex flexDir="row" gap={2}>
+          <Logo />
+        </Flex>
+      </GridItem>
       <GridItem bgColor="blue">1</GridItem>
       <GridItem bgColor="green">2</GridItem>
     </Grid>
