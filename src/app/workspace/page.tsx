a34e5fc942160841f7dac03b0226e79e3ee4efae
Daniel Steman <daniel-steman@live.nl>
2024-04-16 20:03:47 +0200

    add drawer

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,26 +1,50 @@
 "use client";
 
-import { Box, Flex, IconButton, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Drawer,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { UploadFile } from "../upload-file";
 import Logo from "../components/logo";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
 const Workspace = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box>
-      <Flex flexDir="row" p={2} px={4} bgColor={"slate.100"} align="center">
-        <Logo />
-        <Spacer />
-        <IconButton
-          p={0}
-          size="md"
-          icon={<HamburgerIcon />}
-          aria-label={"menu"}
-          fontSize="24px"
-        ></IconButton>
-      </Flex>
-      <UploadFile />
-    </Box>
+    <>
+      <Box>
+        <Flex flexDir="row" p={2} px={4} bgColor={"slate.100"} align="center">
+          <Logo />
+          <Spacer />
+          <IconButton
+            onClick={onOpen}
+            p={0}
+            size="md"
+            icon={<HamburgerIcon />}
+            aria-label={"menu"}
+            fontSize="24px"
+          ></IconButton>
+        </Flex>
+        <UploadFile />
+      </Box>
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">
+            Create a new account
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
