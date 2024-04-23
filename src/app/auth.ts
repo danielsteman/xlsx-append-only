248feb7fc6b792715a08e63a1bb5251876e62455
Daniel Steman <daniel-steman@live.nl>
2024-04-23 15:12:23 +0200

    add user

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -17,11 +17,22 @@ export const lucia = new Lucia(adapter, {
       secure: process.env.NODE_ENV === "production",
     },
   },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      username: attributes.username,
+    };
+  },
 });
 
 // IMPORTANT!
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
   }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
 }
